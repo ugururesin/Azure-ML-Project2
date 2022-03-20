ecbf69e6842724cee3b655843bcd6843df7b3c8e
ugur <55594186+ugururesin@users.noreply.github.com>
2022-03-20 17:20:07 +0300

    initial commit

--- benchmark.sh
+++ benchmark.sh
@@ -70,4 +70,4 @@
 # Total:        114  160  28.0    172     199#
 
 
- ab -n 10 -v 4 -p data.json -T 'application/json' -H 'Authorization: Bearer REPLACE_WITH_KEY' http://REPLACE_WITH_API_URL/score
+ ab -n 10 -v 4 -p data.json -T 'application/json' -H 'Authorization: Bearer MiRihyeE8gEq1RhXMHVth8sVZKuCNAVL' http://31829375-1f34-4c72-8975-fa0f9cbc0f50.southcentralus.azurecontainer.io/score
